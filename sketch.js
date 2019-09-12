07cafc210503a9a98f471c31d38b0289b7acf9af
Audrey McLellan <audrey15mclellan@gmail.com>
2019-09-12 12:09:46 -0500

    Creative Libierty

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+    createCanvas(400, 269);
+    background(240,230,75);
+    fill(175, 0, 25);
+    rect(1, 141, 398, 127);
+    noStroke();
+    fill(0, 255, 0, 0);
+    rect(50, 50, 50, 50);
+    stroke(0);
+    strokeWeight(7);
+    noFill();
+    arc(199, 140, 420, 10, radians(0), radians (180));
+    line(0, 185, 398, 180);
+    line(0, 217, 398, 212);
+    strokeWeight(20);
+    ellipse(70, 75, 100, 50);
+    strokeWeight(7); 
+    fill(0);
+    ellipse(0, 0, 50, 50);
+    ellipse(100, 0, 50, 50);
+    ellipse(180, 40, 100, 50);
+    ellipse(200, 90, 75, 50);
+    ellipse(275, 95, 50, 50);
+    arc(80, 150, 50, 50, 60, 12.3);
+    arc(175, 150, 60, 45, 60, 12.3);
+    arc(300, 150, 50, 50, 60, 12.3);
+    ellipse(350, 80, 35, 35);
+    ellipse(320, 30, 70, 50);
+}
+
+function draw() {
+
+}
